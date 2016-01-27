ca4c2885e8d88d24f2da6a2bd4e0013c9a11571e
Eike Kettner <eike.kettner@posteo.de>
2016-01-27 20:07:39 +0100

    more copy&paste from the book

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -7,12 +7,49 @@ import fpinscala.testing.Prop._
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  def char(c: Char): Parser[Char]
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  def numA: Parser[Int] = char('a').many.map(_.size)
+
+  def succeed[A](a: A) = string("").map(_ => a)
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def many1[A](p: Parser[A]): Parser[List[A]]
+
+  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)] 
+
+  def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
+    product(p, p2).map { case (a,b) => f(a,b) }
+
   case class ParserOps[A](p: Parser[A]) {
-
-
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def many: Parser[List[A]] = self.many(p)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def slice = self.slice(p)
+    def **[B](p2: Parser[B]) = self.product(p, p2)
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in) {s => run(p1)(s) == run(p2)(s) }
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
   }
 }
 
